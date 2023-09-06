ac60ab3182cc6895951500bc213dcb9356ac5492
KaalaAadmi <arnav264@gmail.com>
2023-09-06 13:32:00 +0100

    Binary Search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,16 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-
+  let low=0;
+  let high=haystack.length
+  while(low<high){
+    let mid=Math.floor((low+high)/2)
+    let value=haystack[mid]
+    if(value===needle){
+      return true
+    }else if(value>needle){
+      high=mid
+    }else{
+      low=mid+1
+    }
+  }
+  return false
 }
